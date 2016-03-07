a3e98d5600f3f37ce9fdfa3f652386394d36ff8b
duffrind <duffrind@uwplatt.edu>
2016-03-06 19:32:14 -0500

    Initial Commit

--- web.py
+++
@@ -1,9 +0,0 @@
-from scrapy.item import Item, Field
-from scrapy.spider     import BaseSpider
-from scrapy.selector    import HtmlXPathSelector
-#from nettuts.items  import NettutsItem
-from scrapy.http    import Request
-
-class KanjidamageItem(Item):
-	#
-
